6e1bc98e9850182ded2e470f5391a85dac56a892
chetansy <62282151+chetansy@users.noreply.github.com>
2020-07-03 17:51:53 +0530

    Create init.R

---
+++ init.R
@@ -0,0 +1,32 @@
+#
+# Example R code to install packages
+# See https://cran.r-project.org/doc/manuals/R-admin.html#Installing-packages for details
+#
+
+###########################################################
+# Update this line with the R packages to install:
+
+my_packages = c(
+	"shinydashboard",
+	"shinycssloaders",
+	"forecast",
+  "padr",
+  "imputeTS",
+  "readxl",
+  "dplyr",
+  "ggplot2",
+  "ECharts2Shiny"
+  
+)
+
+###########################################################
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+  else {
+    cat(paste("Skipping already installed package:", p, "\n"))
+  }
+}
+invisible(sapply(my_packages, install_if_missing)
