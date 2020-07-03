221b9b737392d1c368457c7c70db3a9b95e1f673
chetansy <62282151+chetansy@users.noreply.github.com>
2020-07-03 18:24:34 +0530

    Update init.R

--- init.R
+++ init.R
@@ -7,16 +7,9 @@
 # Update this line with the R packages to install:
 
 my_packages = c(
-	"shinydashboard",
-	"shinycssloaders",
-	"forecast",
-  "padr",
-  "imputeTS",
-  "readxl",
-  "dplyr",
-  "ggplot2",
-  "ECharts2Shiny"
-  
+	"tm",
+	"wordcloud",
+	"memoise"
 )
 
 ###########################################################
@@ -29,4 +22,4 @@ install_if_missing = function(p) {
     cat(paste("Skipping already installed package:", p, "\n"))
   }
 }
-invisible(sapply(my_packages, install_if_missing)
+invisible(sapply(my_packages, install_if_missing))
