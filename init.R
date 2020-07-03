078ab7a578f8d5f7c18b025aaa524bd6cc1f6303
chetansy <62282151+chetansy@users.noreply.github.com>
2020-07-03 19:26:05 +0530

    Update init.R

--- init.R
+++ init.R
@@ -6,7 +6,7 @@
 # Update this line with the R packages to install:
 
 my_packages = c(
-	"shinythemes"
+	"shinythemes","shinydashboard","shinycssloaders","forecast","padr","imputeTS","readxl","dplyr","DT","ggplot2","shinyWidgets"
 )
 
 ###########################################################
